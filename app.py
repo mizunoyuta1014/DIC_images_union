e3c9e39a87bf21ba1ef09d5830ba12acfdac9879
mizunoyuta1014 <103983747+mizunoyuta1014@users.noreply.github.com>
2023-11-06 23:52:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ def gather(parent,output):
     gather_images(parent, output)
 
 st.title('DIC画像をまとめる')
+st.write("ソースコード [Github](https://github.com/mizunoyuta1014/DIC_images_union)")
 
 parent_directory = st.text_input('親フォルダのパスを入力してください。')
 output_directory = st.text_input('出力先のフォルダを入力してください。')
 
 button = st.button('ファイルをまとめます')
 if button:
-    st.spinner('処理中です...')
-    gather(parent_directory, output_directory)
-    st.write("処理が完了しました。" + output_directory + "に出力されました。")
+    with st.spinner('処理中です...'):
+        gather(parent_directory, output_directory)
+        st.write("処理が完了しました。" + output_directory + "に出力されました。")
